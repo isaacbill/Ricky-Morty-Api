780e76443ae88a01df10642f26b72bdd18bf4ae9
isaac bill <isaacbill247@gmail.com>
2024-02-16 19:11:22 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
+import Filters from "./components/Filters/Filters";
+import Cards from "./components/Cards/Cards";
+
+function App() {
+  let [pageNumber, setPageNumber] = useState(1);
+  let [fetchedData, updateFetchedData] = useState([])
+  let {info, results} = fetchedData;
+
+  let api =`https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+  useEffect(()=>{
+    (async function(){
+      let data = await fetch(api).then((res)=>res.json());
+      updateFetchedData(data);
+    })();
+  },[api])
+  return (
+    <div className="App">
+      <h1 className="text-center ubuntu my-5">
+        Ricky & Morty <span className="text-primary">App</span> </h1>
+    
+    <div className="container">
+      <div className="row">
+      <div className="col-3">
+        <Filters/>
+      </div>
+      <div className="col-8">
+       <div className="row">
+        <Cards results={results}/>
+        
+        </div>
+      </div>
+      </div>
+    </div>
+    </div> 
+    
+  );
+}
+
+export default App; 
