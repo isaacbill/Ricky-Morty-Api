436623071484c9b4c3a16d44e74c0a9382c5b96d
isaac bill <isaacbill247@gmail.com>
2024-02-16 23:48:53 +0300

    filter complete

--- src/App.js
+++ src/App.js
@@ -10,11 +10,14 @@ import Search from './components/Search/Search';
 function App() {
   let [pageNumber, setPageNumber] = useState(1);
   let [ search, setSearch] = useState("");
+  let [ status, setStatus] = useState("");
+  let [ gender, setGender] =useState("")
+  let [ species, setSpecies] =useState("")
 
   let [fetchedData, updateFetchedData] = useState([])
   let {info, results} = fetchedData;
-
-  let api =`https://rickandmortyapi.com/api/character/?page=${pageNumber} &name=${search}`;
+ 
+  let api =`https://rickandmortyapi.com/api/character/?page=${pageNumber} &name=${search}&status=${status}&gender=${gender}&species=${species}`;
   useEffect(()=>{
     (async function(){
       let data = await fetch(api).then((res)=>res.json());
@@ -26,11 +29,11 @@ function App() {
       <h1 className="text-center ubuntu my-5">
         Ricky & Morty <span className="text-primary">App</span> </h1>
     
-   <Search setSearch= {setSearch}/>
+   <Search  setPageNumber ={ setPageNumber} setSearch= {setSearch}/>
     <div className="container">
       <div className="row">
       <div className="col-3">
-        <Filters/>
+        <Filters setSpecies={setSpecies} setGender= {setGender} setStatus={setStatus} setPageNumber={setPageNumber}/>
       </div>
       <div className="col-8">
        <div className="row">
--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import Status from './Category/status'
+import Gender from './Category/Gender'
+import Species from './Category/Species'
 
-const Filters = () => {
+const Filters = (setStatus, setPageNumber ,setGender, setSpecies) => {
+  let clear = () => {
+    setStatus("");
+    setGender("");
+    setSpecies("");
+    setPageNumber(1);
+    window.location.reload(false);
+  };
   return (
-    <div>Filters</div>
+    <div   className='col-3'>
+    <div className='text-center fw-bold fs-4 mb-2'>Filters</div>
+    <div 
+    onClick={clear}
+     style={{cursor: "pointer"}}
+     className='text-center text-primary text-decoration-underline'>
+       Clear Filters
+       </div>
+       <div className="accordion" id="accordionExample">
+        <Status
+          setPageNumber={setPageNumber}
+         setStatus={setStatus}
+        />
+        <Species
+          setPageNumber={setPageNumber}
+          setSpecies={setSpecies}
+        />
+        <Gender
+          setPageNumber={setPageNumber}
+          setGender={setGender}
+        />
+      </div>
+    </div>
   )
 }
 
